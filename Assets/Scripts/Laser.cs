fddc8c49bad62c2802d26b295e23560b4a5fe38a
Rhett <rhetthay66@gmail.com>
2024-10-17 23:56:20 -0400

    Created a new Laser script, attached it to the Laser prefab. This script makes the lasers move upward after pressing the Space key each time.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float _speed = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+}
