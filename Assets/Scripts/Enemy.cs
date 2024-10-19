f9bff3183aec9446899bab62ed3da98f68aa2b71
Rhett <rhetthay66@gmail.com>
2024-10-19 00:10:59 -0400

    Created Enemy behavior that has the enemies moving downward on the screen. Also, when the enemies move too far down the screen, they will spawn at the top of the screen in a random position.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4f;
+    [SerializeField] private float _minX = -9.5f;
+    [SerializeField] private float _maxX = 9.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y <= -6f)
+        {
+            float randomX = Random.Range(_minX, _maxX);
+            transform.position = new Vector3(randomX, 8f, 0);
+        }
+    }
+}
