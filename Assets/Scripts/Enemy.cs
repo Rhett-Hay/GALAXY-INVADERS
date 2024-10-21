5fe2114ec0156af3dfeba76798960ae00bcbdd22
Rhett <rhetthay66@gmail.com>
2024-10-21 02:21:50 -0400

    Added collisions for the Player, Enemy, and Laser. The Enemy game object is destroyed when colliding into either Player or Lasers.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -25,4 +25,25 @@ public class Enemy : MonoBehaviour
             transform.position = new Vector3(randomX, 8f, 0);
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // IF other equals the Player's tag
+        // Damage the player,
+        // AND destroy ourselves
+
+        if (other.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+
+        // IF other equals Laser tag
+        // Destroy Laser and ourselves
+
+        if (other.CompareTag("Laser"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
 }
