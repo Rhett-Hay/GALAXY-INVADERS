e94369a679783617f0868c3b5461aca4077d830b
Rhett <rhetthay66@gmail.com>
2024-10-24 19:45:11 -0400

    Created a Triple Shot Powerup object that can be collected when the Player collides with it.

---
+++ Assets/Scripts/PowerUps.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y <= -6f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.transform.GetComponent<Player>();
+
+            if (player != null)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
