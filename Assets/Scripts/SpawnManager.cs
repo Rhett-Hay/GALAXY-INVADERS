0983590f7da644879a738a2401da0dd79218c0ba
Rhett <rhetthay66@gmail.com>
2024-10-27 19:48:04 -0400

    Added animations to the Triple Shot powerups. Also, created a spawn routine where the powerups will spawn every 3 - 8 seconds.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,13 +7,15 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private float _Xpos;
     [SerializeField] private GameObject _enemyContainer;
+    [SerializeField] private GameObject _tripleShotPowerupPrefab;
 
     private bool _stopSpawning;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
     }
 
     // Update is called once per frame
@@ -22,7 +24,7 @@ public class SpawnManager : MonoBehaviour
         
     }
 
-    IEnumerator SpawnRoutine()
+    IEnumerator SpawnEnemyRoutine()
     {
         yield return new WaitForSeconds(2f);
 
@@ -33,6 +35,19 @@ public class SpawnManager : MonoBehaviour
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator SpawnPowerupRoutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (_stopSpawning == false)
+        {
+            float randomX = Random.Range(-_Xpos, _Xpos);
+            Vector3 posToSpawn = new Vector3(randomX, transform.position.y, 0);
+            Instantiate(_tripleShotPowerupPrefab, posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(3f, 8f));
         }
     }
 
