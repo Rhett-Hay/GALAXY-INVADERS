6e1217a933d2d03e1f9f42843fff7e928df28b79
Rhett <rhetthay66@gmail.com>
2024-10-31 02:57:46 -0400

    Game Over text appears onscreen when the Player loses all of their lives.

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -9,11 +9,13 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TMPro.TMP_Text _scoreText;
     [SerializeField] private Sprite[] _liveSprites;
     [SerializeField] private Image _livesImage;
+    [SerializeField] private TMPro.TMP_Text _gameOverText;
 
     // Start is called before the first frame update
     void Start()
     {
         _scoreText.text = "SCORE: " + 0;
+        _gameOverText.gameObject.SetActive(false);
     }
 
     public void UpdateScore(int playerScore)
@@ -24,5 +26,15 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _livesImage.sprite = _liveSprites[currentLives];
+
+        if (currentLives == 0)
+        {
+            GameOverSequence();
+        }
+    }
+
+    private void GameOverSequence()
+    {
+        _gameOverText.gameObject.SetActive(true);
     }
 }
